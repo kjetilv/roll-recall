3e993b380a211ba92dc42b87648faf8801530802
Maja Bratseth <mbratset@zenior.no>
2013-05-09 22:42:33 +0200

    Add button works

--- RollRecall/RollRecall/RRDetailViewController.h
+++ RollRecall/RollRecall/RRDetailViewController.h
@@ -1,4 +1,11 @@
 #import <Foundation/Foundation.h>
 
+@class RRDetailViewController;
+
+@protocol RRDetailViewControllerDelegate
+- (void)detailViewControllerDidFinish:(RRDetailViewController *)controller;
+@end
+
 @interface RRDetailViewController : UIViewController
+@property(nonatomic, strong) id<RRDetailViewControllerDelegate> delegate;
 @end
