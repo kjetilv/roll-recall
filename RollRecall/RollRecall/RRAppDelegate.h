6f7db4341db1986fcdcede0649ebf123c4afd8a3
Maja Bratseth <mbratset@zenior.no>
2013-05-09 21:44:44 +0200

    Empty project

---
+++ RollRecall/RollRecall/RRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RRAppDelegate.h
+//  RollRecall
+//
+//  Created by maja on 05/09/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RollRecall/RollRecall/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  RollRecall
+//
+//  Created by maja on 05/09/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RRAppDelegate class]));
+    }
+
+}
